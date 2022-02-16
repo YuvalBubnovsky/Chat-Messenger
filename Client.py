a6f14dafe6f4ad56641e3761a51d465ac7a9aea9
Yonatan Ratner <yonaratner@gmail.com>
2022-02-16 19:00:28 +0200

    clients to server communication done, client to client communication unsolved

---
+++ Client.py
@@ -0,0 +1,46 @@
+"""
+1. connects to Server on Port 50000 - allocator port
+2. connects to a free port ( 55000 - 55015 ) which he was told by the allocator
+3. uses the chat, etc...
+"""
+
+from socket import *
+from threading import Thread
+import sys
+
+
+# We receive user input through the command line as CommandLineClient.py server-host server-port filename
+serverHost = "10.0.0.2"
+serverPort = 55000
+username = sys.argv[1]
+
+
+def response_thread(client_socket):
+    response = (client_socket.recv(4096)).decode()
+    print(response)
+
+
+def message_thread(client_socket):
+    message = input()
+    client_socket.send(message.encode())
+
+
+# Next step is to parse the arguments and create a TCP socket, we then attempt to connect to the designated server
+try:
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    print("Socket Created")
+    clientSocket.connect((serverHost, serverPort))
+    print("Connected to: " + serverHost + " on port: " + str(serverPort))
+    clientSocket.send(username.encode())
+    while True:
+        t1 = Thread(target=response_thread, args=[clientSocket])
+        t2 = Thread(target=message_thread, args=[clientSocket])
+        t1.start()
+        t2.start()
+
+
+except IOError:
+    print("An error has occurred, please try again\r\n Closing client connection")
+    sys.exit(1)
+
+clientSocket.close()
--- Server.py
+++ Server.py
@@ -2,11 +2,80 @@ import sys
 from socket import *
 from threading import *
 
+""" GLOBALS """
+
 PING_PORT = 50000  # port that handles initial connection
+SERVER_PORT = 55000  # start of server ports as per description
+client_list = []
 
-allocating_socket = socket(AF_INET, SOCK_STREAM)
-allocating_socket.listen(15)  # we allow 15 connections max, so at worst we will need to multi-thread 15 requests.
+""" ******* """
 
-taken_ports = {False, False, False, False, False, False, False, False, False, False, False, False, False, False, False}
+allocator_socket = socket(AF_INET, SOCK_STREAM)
+allocator_socket.bind(('', SERVER_PORT))
+allocator_socket.listen(15)  # we allow 15 connections max, so at worst we will need to queue 15 requests.
+
+print("Ready to serve on port ", SERVER_PORT)
 
 
+def BroadcastToOne(message, connection):
+    try:
+        connection.send(message.encode())
+    except IOError:
+        connection.close()
+        client_list.remove(connection)
+
+
+def BroadcastToAll(message, connection):
+    for client in client_list:
+        if client[0] != connection:
+            try:
+                client[0].send(message.encode())
+
+            except IOError:
+                client[0].close()
+                client_list.remove(client)
+
+
+def find_by_name(name):
+    for client in client_list:
+        if client[1] == name:
+            return client[0]
+    return "all" #TODO: remove this
+
+
+def Threader(connection: socket, addr, name):
+    while True:
+        try:
+            message = connection.recv(4096).decode()
+
+            if message:
+                protocol = message[1]
+                content = message[0]
+                broadcast_msg = "<" + name + ">: " + str(content)
+                if protocol is all:
+                    print(broadcast_msg)
+                    BroadcastToAll(broadcast_msg, connection)
+                else:
+                    connect = find_by_name(protocol)
+                    BroadcastToOne(broadcast_msg, connect)
+
+        except IOError:
+            # Close client socket
+            connection.close()
+
+
+while True:
+    try:
+        connection_socket, address = allocator_socket.accept()
+        username = connection_socket.recv(4096).decode()
+        client_list.append((connection_socket, username))
+        # print("Connection from " + str(address))
+        thread = Thread(target=Threader, args=(connection_socket, address, username))
+        thread.start()
+
+    except OSError:
+        print("Something went wrong before threading the request!, the run will now stop.\n")
+        break
+
+allocator_socket.close()
+sys.exit()
