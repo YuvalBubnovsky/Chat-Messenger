4c805f07631a29c6580e1a18883ad8b4ffe3dfcd
Yuval Bubnovsky <yuval.bubnov@gmail.com>
2022-02-21 23:03:21 +0200

    Initial UDP work

--- Client.py
+++ Client.py
@@ -7,12 +7,46 @@
 from socket import *
 from threading import Thread
 import sys
-
+import os
+import time
 
 serverHost = "192.168.1.215"
 serverPort = 55000
+udp_port = 55001
 username = sys.argv[1]
+MTU = 1500
 
+
+# ================================================================ #
+#                               UDP                                #
+# ================================================================ #
+
+# TODO: Make this threaded
+def request_file():
+    File_Socket = socket(AF_INET, SOCK_DGRAM)
+    File_Socket.settimeout(10)
+
+    File_Socket.sendto("HANDSHAKE".encode(), (serverHost, udp_port))
+    server_response = get_response(File_Socket)
+    if server_response == "ACK":
+        print("Server Is Live! You Can Receive Files!\n")
+    while True:
+        File_Socket.sendto("SEND".encode(),(serverHost, udp_port))
+        server_response = get_response(File_Socket)
+        if server_response == "ACK":
+
+
+def get_response(sock):
+    try:
+        rec_msg = sock.recvfrom(MTU)
+        return rec_msg[0]
+    except IOError:
+        return "An Error Occured, Please Try Again!\n"
+
+
+# ================================================================ #
+#                             END UDP                              #
+# ================================================================ #
 
 def response_thread(client_socket):
     response = (client_socket.recv(4096)).decode()
@@ -22,6 +56,45 @@ def response_thread(client_socket):
 def message_thread(client_socket):
     message = input()
     client_socket.send(message.encode())
+
+
+# TODO: This also needs attention after were done with server side
+def calculate_checksum(data):
+    nleft = len(data)
+    sum = 0
+    pos = 0
+    while nleft > 1:
+        sum = ord(data[pos]) * 256 + (ord(data[pos + 1]) + sum)
+        pos = pos + 2
+        nleft = nleft - 2
+    if nleft == 1:
+        sum = sum + ord(data[pos]) * 256
+
+    sum = (sum >> 16) + (sum & 0xFFFF)
+    sum += (sum >> 16)
+    sum = (~sum & 0xFFFF)
+
+    return sum
+
+
+def verify_checksum(data):
+    nleft = len(data)
+    sum = 0
+    pos = 0
+    while nleft > 1:
+        sum = ord(data[pos]) * 256 + (ord(data[pos + 1]) + sum)
+        pos = pos + 2
+        nleft = nleft - 2
+    if nleft == 1:
+        sum = sum + ord(data[pos]) * 256
+
+    cksum = calculate_checksum(data)
+    flag = (cksum) ^ (sum)
+    for bit in flag:
+        if flag[bit] == 0:
+            return False
+        else:
+            return True
 
 
 # TODO: Switch the loop structure so we don't create INF threads
--- Server.py
+++ Server.py
@@ -2,10 +2,15 @@ import sys
 from socket import *
 from threading import *
 
+# TODO: Add command to display all connected clients
+# TODO: Add "<user> has connected to chat!"
+
 """ GLOBALS """
 
 PING_PORT = 50000  # port that handles initial connection
 SERVER_PORT = 55000  # start of server ports as per description
+FILE_PORT = 55001
+MTU = 1500
 client_list = []
 
 """ ******* """
@@ -14,8 +19,34 @@ allocator_socket = socket(AF_INET, SOCK_STREAM)
 allocator_socket.bind(('', SERVER_PORT))
 allocator_socket.listen(15)  # we allow 15 connections max, so at worst we will need to queue 15 requests.
 
-print("Ready to serve on port ", SERVER_PORT)
+print("Ready to serve on port \n", SERVER_PORT)
 
+# ================================================================ #
+#                               UDP                                #
+# ================================================================ #
+
+#TODO: Make this threaded
+#TODO: Add check if user is on client_list while requesting file, deny if not
+
+file_socket = socket(AF_INET, SOCK_DGRAM)
+file_socket.bind(('', FILE_PORT))
+file_socket.listen(15)
+
+print("File port is ready on \n", FILE_PORT)
+
+while True:
+    msg, client_addr = file_socket.recvfrom(MTU)
+    if msg.decode() == "HANDSHAKE":
+        file_socket.sendto("ACK".encode(),client_addr)
+        print("UDP Connection Established With %s:%s" % (client_addr[0], client_addr[1]))
+    elif msg.decode() == "SEND":
+
+
+
+
+# ================================================================ #
+#                             END UDP                              #
+# ================================================================ #
 
 def BroadcastToOne(message, connection):
     try:
@@ -40,7 +71,46 @@ def find_by_name(name):
     for client in client_list:
         if client[1] == name:
             return client[0]
-    return "all" #TODO: remove this
+    return "all"  # TODO: remove this
+
+
+# TODO : Verify this & Figure it out
+def calculate_checksum(data):
+    nleft = len(data)
+    sum = 0
+    pos = 0
+    while nleft > 1:
+        sum = ord(data[pos]) * 256 + (ord(data[pos + 1]) + sum)
+        pos = pos + 2
+        nleft = nleft - 2
+    if nleft == 1:
+        sum = sum + ord(data[pos]) * 256
+
+    sum = (sum >> 16) + (sum & 0xFFFF)
+    sum += (sum >> 16)
+    sum = (~sum & 0xFFFF)
+
+    return sum
+
+
+def verify_checksum(data):
+    nleft = len(data)
+    sum = 0
+    pos = 0
+    while nleft > 1:
+        sum = ord(data[pos]) * 256 + (ord(data[pos + 1]) + sum)
+        pos = pos + 2
+        nleft = nleft - 2
+    if nleft == 1:
+        sum = sum + ord(data[pos]) * 256
+
+    cksum = calculate_checksum(data)
+    flag = (cksum) ^ (sum)
+    for bit in flag:
+        if flag[bit] == 0:
+            return False
+        else:
+            return True
 
 
 def Threader(connection: socket, addr, name):
