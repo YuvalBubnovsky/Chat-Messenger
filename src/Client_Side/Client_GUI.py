f2d6c607205f6d32a01b061dd9aa0d6702076518
Yuval Bubnovsky <yuval.bubnov@gmail.com>
2022-03-04 01:07:11 +0200

    linking gui to logic

--- src/Client_Side/Client_GUI.py
+++ src/Client_Side/Client_GUI.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 from tkinter.ttk import Labelframe
 import Controller
 
+
 # TODO: Add DC functions
 
 class Client_GUI:
@@ -12,11 +13,12 @@ class Client_GUI:
         self.root.title("Client")
         self.root.geometry("640x320")
         self.root.resizable(width=False, height=False)
+        self.controller = Controller.Controller(None, None, None, None, None, None)
         self.build_gui()
-        self.controller = Controller.Controller(None, None)
         self.root.mainloop()
 
     # TODO: connect all functions to controller
+    # TODO: Button State Switch
 
     def build_gui(self):
         self.build_top_frame()
@@ -33,29 +35,37 @@ class Client_GUI:
     # def clear_text(self):  # For clearing user text-box after input
     #   self.delete(1.0, 'end')
 
-    def on_login(self):
-        login_screen = Toplevel(self.root)
-        login_screen.title("Login")
-        login_screen.geometry("300x250")
+    def on_login(self, button: Button):
+        if button.cget('text') == "Login":
+            button.config(text="Logout")
+            login_screen = Toplevel(self.root)
+            login_screen.title("Login")
+            login_screen.geometry("300x250")
 
-        username = StringVar()
-        user_address = StringVar()
+            username = StringVar()
+            user_address = StringVar()
 
-        # Set username label
-        username_label = Label(login_screen, text="Username  ")
-        username_label.place(relx=0.5, rely=0.2, anchor=CENTER)
+            # Set username label
+            username_label = Label(login_screen, text="Username  ")
+            username_label.place(relx=0.5, rely=0.2, anchor=CENTER)
 
-        username_entry = Entry(login_screen, textvariable=username)
-        username_entry.place(relx=0.5, rely=0.3, anchor=CENTER)
+            username_entry = Entry(login_screen, textvariable=username)
+            username_entry.place(relx=0.5, rely=0.3, anchor=CENTER)
 
-        address_label = Label(login_screen, text="Address  ")
-        address_label.place(relx=0.5, rely=0.4, anchor=CENTER)
+            address_label = Label(login_screen, text="Address  ")
+            address_label.place(relx=0.5, rely=0.4, anchor=CENTER)
 
-        address_entry = Entry(login_screen, textvariable=user_address, show='*')
-        address_entry.place(relx=0.5, rely=0.5, anchor=CENTER)
+            address_entry = Entry(login_screen, textvariable=user_address)
+            address_entry.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-        Label(login_screen, text="").pack()
-        Button(login_screen, text="Connect", width=10, height=1,command= lambda: self.controller.connect(username_entry, address_entry, login_screen)).place(relx=0.5, rely=0.7, anchor=CENTER)
+            Label(login_screen, text="").pack()
+            Button(login_screen, text="Connect", width=10, height=1,
+                   command=lambda: self.controller.connect(username_entry, address_entry, login_screen)).place(relx=0.5,
+                                                                                                               rely=0.7,
+                                                                                                               anchor=CENTER)
+        else:
+            button.config(text="Login")
+            self.controller.disconnect()
 
     def on_whois(self):
         pass
@@ -71,15 +81,24 @@ class Client_GUI:
         top_frame.pack(padx=15)
 
         # TODO: Add state for button to switch once the user logged in and back out
-        login_button = Button(top_frame, text="Login", width=10, command=self.on_login).pack(side='left', padx=5)
+        login_button = Button(top_frame, text="Login", width=10,
+                              command=lambda: self.on_login(login_button))
+        login_button.pack(side='left', padx=5)
 
         # TODO: Add state for button to activate only once user logged in, same for show files
-        show_online_button = Button(top_frame, text="Who's Online", width=10, command=self.on_whois, state=DISABLED).pack(
-            side='left', padx=5)
+        show_online_button = Button(top_frame, text="Who's Online", width=10, command=self.on_whois,
+                                    state=DISABLED)
+        show_online_button.pack(side='left', padx=5)
+        self.controller.set_online_button(show_online_button)
+
         show_server_files_button = Button(top_frame, text="Show Files", width=10, command=self.on_showfile,
-                                          state=DISABLED).pack(side='left', padx=5)
-        clear_button = Button(top_frame, text="Clear", width=10, command=self.on_clear, state=DISABLED).pack(side='left',
-                                                                                                        padx=5)
+                                          state=DISABLED)
+        show_server_files_button.pack(side='left', padx=5)
+        self.controller.set_files_button(show_server_files_button)
+
+        clear_button = Button(top_frame, text="Clear", width=10, command=lambda: self.controller.clear_text(), state=DISABLED)
+        clear_button.pack(side='left', padx=5)
+        self.controller.set_clear_button(clear_button)
 
     def build_mid_frame(self):
         # TODO: How to print to this?
@@ -92,10 +111,12 @@ class Client_GUI:
         display_box.config(state=DISABLED)
         display_box.pack(side='left', padx=10)
         scrollbar.pack(side='left', fill='y')
+        self.controller.set_chat_box(display_box)
 
         # TODO: populate list properly and bind double click functions
         user_list = Listbox(chat_frame, height=10, selectmode=SINGLE)
         user_list.pack(side='right', padx=5)
+        self.controller.set_user_list(user_list)
 
     def build_bottom_frame(self):
         bottom_frame = Labelframe(self.root, text='Enter Message')
@@ -103,6 +124,7 @@ class Client_GUI:
 
         # TODO: add a bind for sending messages using return key
         chat_area.pack(side='top', pady=5)
+        self.controller.set_user_input(chat_area)
 
         bottom_frame.pack(side='top')
 
--- src/Client_Side/Controller.py
+++ src/Client_Side/Controller.py
@@ -7,11 +7,16 @@ from Server_Side import Client
 
 class Controller:
 
-    def __init__(self, chat_box, user_input):
+    def __init__(self, chat_box, user_input, online_button, files_button, clear_button, user_list):
         self.client = Client.Client('127.0.0.1', 55000, 55001, " ", 1500, 5, True)
         # TODO: Is server needed?
         self.chat_box = chat_box
         self.user_input = user_input
+        self.online_button = online_button
+        self.files_button = files_button
+        self.clear_button = clear_button
+        self.user_list = user_list
+        self.is_connected = False
 
     def set_chat_box(self, new_box: Text):
         self.chat_box = new_box
@@ -19,6 +24,37 @@ class Controller:
     def set_user_input(self, new_input):
         self.user_input = new_input
 
+    def set_online_button(self, new_button: Button):
+        self.online_button = new_button
+
+    def set_files_button(self, new_button: Button):
+        self.files_button = new_button
+
+    def set_clear_button(self, new_button: Button):
+        self.clear_button = new_button
+
+    def set_user_list(self, new_list: Listbox):
+        self.user_list = new_list
+
+    def update_state(self):
+        for widget in [self.user_input, self.online_button, self.files_button, self.clear_button, self.user_list]:
+            if self.is_connected:
+                widget.config(state=NORMAL)
+            else:
+                widget.config(state=DISABLED)
+
+    def clear_all(self):
+        self.chat_box.config(state=NORMAL)
+        self.user_list.config(state=NORMAL)
+        self.chat_box.delete("1.0", "end")
+        self.user_list.delete(0,END)
+        self.user_list.config(state=DISABLED)
+        self.chat_box.config(state=DISABLED)
+
+    def clear_text(self):
+        self.chat_box.config(state=NORMAL)
+        self.chat_box.delete("1.0", "end")
+        self.chat_box.config(state=DISABLED)
 
     def connect(self, user_entry: Entry, addr_entry: Entry, login: Toplevel):
         user_name = user_entry.get()
@@ -30,19 +66,18 @@ class Controller:
         except OSError:
             messagebox.showinfo("Error!", "An Error Has Occured, Please Try Again!")
             return
-        user_entry.delete(0,END)
+        user_entry.delete(0, END)
         user_entry.insert(0, " ")
-        addr_entry.delete(0,END)
+        addr_entry.delete(0, END)
         addr_entry.insert(0, " ")
         login.withdraw()
+        self.is_connected = True
+        self.update_state()
+        self.chat_box.config(state=NORMAL)
+        self.chat_box.insert('end', "Connected To Server!")
+        self.chat_box.config(state=DISABLED)
 
     def disconnect(self):
-        pass # TODO: Implement this with the state button of login/logout
-
-
-
-
-
-
-
-
+        self.update_state()
+        self.clear_all()
+        pass  # TODO: Implement this with the state button of login/logout
